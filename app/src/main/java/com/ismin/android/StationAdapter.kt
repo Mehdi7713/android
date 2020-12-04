c7d5f9b4406f9f46f8377a3b8d95285df9c5a1b0
unknown <eleve@GCP-PRET-ID4268.emse2000.local>
2020-12-04 19:28:32 +0100

    Android

---
+++ app/src/main/java/com/ismin/android/StationAdapter.kt
@@ -0,0 +1,36 @@
+package com.ismin.android
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class StationAdapter(private val stations: ArrayList<Station>, var clickListner: OnStationItemClickListner) : RecyclerView.Adapter<StationViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StationViewHolder {
+        val row = LayoutInflater.from(parent.context).inflate(R.layout.row_station, parent, false)
+        return StationViewHolder(row)
+    }
+
+    override fun onBindViewHolder(holder: StationViewHolder, position: Int) {
+        val (name, zone, typeRATP) = this.stations[position]
+
+       // holder.txvName.text = name
+      //  holder.txvZone.text = zone
+       // holder.txvTypeRATP.text = typeRATP
+        holder.initialize(this.stations.get(position), clickListner)
+
+    }
+
+    override fun getItemCount(): Int {
+        return this.stations.size
+    }
+
+    fun updateItem(stationsToDisplay: List<Station>) {
+        stations.clear();
+        stations.addAll(stationsToDisplay)
+        notifyDataSetChanged();
+    }
+}
+
+interface OnStationItemClickListner{
+    fun onItemClick(item: Station, position: Int)
+}
