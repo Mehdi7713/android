ddf36eed20c35ba1a70622f91368428e7b694d03
INNOURI Ayoub <70719321+INNOURI@users.noreply.github.com>
2020-12-04 19:48:55 +0100

    Delete Station.kt

--- AppLOUAHEMINNOURI/app/src/main/java/com/example/myapplicationlouaheminnouri/Station.kt
+++
@@ -1,5 +0,0 @@
-package com.ismin.android
-
-import java.io.Serializable
-
-data class Station(val name: String,  val zone: String, val typeRATP: String, val id: Int, val lat: Double, val lon: Double, val favorite:Int) : Serializable
